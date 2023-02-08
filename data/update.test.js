0f01b78d7b72706a719db52d380ad4cf64dd087a
Weng Fei Fung <weffung@ucdavis.edu>
2023-02-07 19:08:40 -0800

    Write test for basic recursing

--- data/update.test.js
+++ data/update.test.js
@@ -38,3 +38,58 @@ describe("The dtree would be for-loop", ()=>{
 
 });
 
+describe("The dtree would be recursive", ()=>{
+
+    const subject = [
+        "a",
+        "b",
+        [1,2,"3",4,"5"]
+    ]
+    describe("Test values", function() {
+        test("Element order is maintained in array that is nested with mixed values", function() {
+            expect(subject[0]).toEqual("a")
+            expect(subject[2].length).toBeGreaterThan(0);
+            expect(subject[2][0]).toEqual(1);
+            expect(subject[2][2]).toEqual("3");
+            expect(subject[2][4]).toEqual("5");
+            // Test Jest is strict with types
+            expect(1).not.toEqual("1")
+        });
+    })
+    describe("Recursion for flattening", function() {
+        test("Flatten nested array", function() {
+            let flattened = [];
+            function recurse(arr, i, flattened) {
+                // Current value is arr[i]
+                // Beginning of array starts with -1 so can increment to 0
+
+                i++;
+                if(i>=arr.length) {
+                    return flattened;
+                } else if(Array.isArray(arr[i])) {
+                    return recurse(arr[i], -1, flattened)
+                } else {
+                    flattened.push(arr[i])
+                    return recurse(arr, i, flattened)
+                }
+
+            }
+            let i = -1;
+            const results = recurse(subject,i,[])
+
+            console.log({results})
+            
+            // Test Jest is strict with types
+            expect(1).toEqual(1)
+        });
+    });
+    
+    // describe("Recursion keeps nested structure but transforms the values", function() {
+    //     test("Transform each value into object {value:_value}", function() {
+            
+            
+    //     });
+    // })
+
+
+});
