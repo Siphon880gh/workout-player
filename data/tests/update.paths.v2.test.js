e60e77da1315b307a0fa275e4af92154db0ca828
Weng Fei Fung <weffung@ucdavis.edu>
2023-02-08 22:32:51 -0800

    Tests for transforming the simple flattened array into File Navigator element information

--- data/tests/update.paths.v2.test.js
+++ data/tests/update.paths.v2.test.js
@@ -106,5 +106,44 @@ describe("The dtree would recursively get transformed and flattened for renderin
         expect(flattened).toEqual(["file","dir","file","file","dir","file"]);
     })
 
+    test("Practical transformation and flattening fits dree file structure", ()=>{
+        let flattened = [];
 
+        function transform(object, level) {
+            return {
+                id: "hash-"+object?.hash, 
+                className: object?.type==="directory"?"dir":"file",
+                parent: object?.path?.match(/.*\/(.*)\/.*\w+/)[1],
+                level: object?.relativePath?.split("/").length - 1,
+                path: object?.relativePath,
+                textContent: object?.name // actually filename from dree
+            }
+        }
+        function recurse(arr, i, flattened) {
+            // Current value is arr[i]
+            // Beginning of array starts with -1 so can increment to 0
+            i++;
+            if(i>=arr.length) {
+                return flattened; // Return final flattened[]
+            } else if(arr[i]?.children?.length) {
+                flattened.push(transform(arr[i])); // Add to flattened[]
+                return recurse(arr[i].children, -1, flattened) // This is FS directory node. Step into its children array.
+            } else {
+                // let transformed = transform(arr[i], ".", -1);
+                flattened.push(transform(arr[i])); // Add to flattened[]
+                return recurse(arr, i, flattened) // Step next
+            }
+    
+        }
+        let i = -1;
+        console.log({tree});
+        flattened = recurse([tree],i,[]); // At object representing FS directory node "/test". Step into its children array
+        flattened.shift(0,1);
+    
+        console.log({flattened})
+    
+        expect(flattened.length).toEqual(6);
+        expect(flattened[0].className).toEqual("file");
+        expect(flattened[1].className).toEqual("dir");
+    })
 });
