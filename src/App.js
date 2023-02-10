c2047195138543012aa015adc721ded11631aa7d
Weng Fei Fung <weffung@ucdavis.edu>
2023-02-09 17:53:18 -0800

    Flex layout for left and right panels

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import './App.css';
 import FileNavigator from "./FileNavigator"
+import FileViewer from "./FileViewer"
+
+import { BrowserRouter } from "react-router-dom";
+import { Route } from "react-router-dom";
+import { Switch } from "react-router-dom";
+
+const Router = BrowserRouter;
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Workout Planner</h1>
+        <h1>Workout Notebook</h1>
       </header>
-      <main>
+      <Router>
+      <main className="flex-row">
         <FileNavigator/>
-
+        <FileViewer/>
       </main>
+      </Router>
     </div>
   );
 }
---
+++ src/FileViewer.js
@@ -0,0 +1,15 @@
+import "./FileViewer.css"
+import {useState, useEffect} from "react";
+
+function FileViewer() {
+
+  useEffect(()=>{
+  })
+
+
+    return (
+      <div className="file-viewer"></div>
+    );
+  }
+
+export default FileViewer;
