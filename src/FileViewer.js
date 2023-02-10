b3dae495fb425961f9f3466e1b2e963c9bd9e0c7
Weng Fei Fung <weffung@ucdavis.edu>
2023-02-09 18:23:50 -0800

    FileReader has access to file to open and props passed from outside router

--- src/FileViewer.js
+++ src/FileViewer.js
@@ -4,16 +4,24 @@ import {useState, useEffect} from "react";
 function FileViewer(props) {
 
   let [passed, setPassed] = useState("");
+  let [relativePath, setRelativePath] = useState("");
 
   useEffect(()=>{
     if(props?.passed) {
       setPassed(props.passed)
     }
+
+    setRelativePath((()=>{
+      return window.location.href.substr(window.location.href.indexOf("/view/")+"/view/".length)
+    })())
   })
 
 
     return (
-      <div className="file-viewer">{passed}</div>
+      <div className="file-viewer">
+        <div>Passed prop: {passed}</div>
+        <div>Relative path from URL: {relativePath}</div>
+      </div>
     );
   }
 
