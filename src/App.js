97795c6772c363ad9c7e51476f30c19e19333f53
Weng Fei Fung <weffung@ucdavis.edu>
2023-02-10 18:35:45 -0800

    Full workout page mode

--- src/App.js
+++ src/App.js
@@ -2,27 +2,38 @@ import './App.css';
 import FileNavigator from "./FileNavigator"
 import FileViewer from "./FileViewer"
 
+import {useState} from "react";
+
 import { BrowserRouter } from "react-router-dom";
 import { Route } from "react-router-dom";
 import { Routes } from "react-router-dom";
 
 const Router = BrowserRouter;
 
+function Header() {
+  return (
+    <header className="App-header">
+      <h1>Workout Notebook</h1>
+    </header>
+  )
+}
+
 function App() {
+  let [showMisc, setShowMisc] = useState(true);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Workout Notebook</h1>
-      </header>
+      {showMisc?<Header/>:""}
       <Router>
       <main className="flex-row">
-        <FileNavigator/>
-      
+        {showMisc?<FileNavigator/>:""}
+        
         <Routes>
           <Route path="/view/*" element={<FileViewer/>}></Route>
         </Routes>
       </main>
       </Router>
+      <span id="toggle-sidebar" onClick={()=>setShowMisc(!showMisc)}>👁</span>
     </div>
   );
 }
