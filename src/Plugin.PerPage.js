99749828c0154fa2cdb8f367f422187b257b1834
Siphon880gh <weng.f.fung@gmail.com>
2024-02-06 06:27:06 -0800

    Plugin commented out but can be extended

--- src/Plugin.PerPage.js
+++ src/Plugin.PerPage.js
@@ -1,6 +1,9 @@
 
 export const runPerPage = () => {
-    document.querySelectorAll("a[target='_blank']").forEach(el=>{
-        el.setAttribute("target", "_self");
-    });
+    // Pointless to make exercise links open in same frame of wrapper-workout
+    // because youtube and google blocked iframe embedding
+    
+    // document.querySelectorAll("a[target='_blank']").forEach(el=>{
+    //     el.setAttribute("target", "_self");
+    // });
 } // runPerPage
