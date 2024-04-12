9511d36bb058369ed4fce0b785fe48d9cfe24d3b
Siphon880gh <weng.f.fung@gmail.com>
2024-04-12 01:21:44 -0700

    UX: CTA, Back to welcome screen, Social icons

--- src/App.js
+++ src/App.js
@@ -20,19 +20,35 @@ function Header() {
 
   return (
     <header className="App-header" style={{display:"flex", justifyContent:"space-between", flexWrap:"wrap", padding:"0 10px"}}>
-      <h1>Workout Notebook</h1>
+      <h1 style={{padding:"10px 0"}}><a href="/" style={{color:"white", textDecoration:"none"}}>Workout Notebook</a></h1>
       
       {!shownCreds
         ?
         (<h2 style={{margin:"10px 0 10px 10px", cursor:"help"}} onClick={()=>{ setShownCreds(true) }}>By Weng</h2>)
         :
         (
-          <div style={{display:"flex", flexDirection:"row", alignItems:"flex-end"}}>
-            <h2 style={{margin:"10px 0"}}>
-            <a target="_blank" href="https://github.com/Siphon880gh" rel="nofollow"><img src="https://img.shields.io/badge/GitHub--blue?style=social&logo=GitHub" alt="Github" data-canonical-src="https://img.shields.io/badge/GitHub--blue?style=social&logo=GitHub" style={{maxWidth:"10ch", verticalAlign:"middle", marginRight:"10px"}}/></a>
-            <a target="_blank" href="https://www.linkedin.com/in/weng-fung/" rel="nofollow"><img src="https://camo.githubusercontent.com/0f56393c2fe76a2cd803ead7e5508f916eb5f1e62358226112e98f7e933301d7/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f4c696e6b6564496e2d626c75653f7374796c653d666c6174266c6f676f3d6c696e6b6564696e266c6162656c436f6c6f723d626c7565" alt="Linked-In" data-canonical-src="https://img.shields.io/badge/LinkedIn-blue?style=flat&amp;logo=linkedin&amp;labelColor=blue" style={{maxWidth:"10ch", verticalAlign:"middle", marginRight:"10px"}}/></a>
-            <a target="_blank" href="https://www.youtube.com/user/Siphon880yt/" rel="nofollow"><img src="https://camo.githubusercontent.com/0bf5ba8ac9f286f95b2a2e86aee46371e0ac03d38b64ee2b78b9b1490df38458/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f596f75747562652d7265643f7374796c653d666c6174266c6f676f3d796f7574756265266c6162656c436f6c6f723d726564" alt="Youtube" data-canonical-src="https://img.shields.io/badge/Youtube-red?style=flat&amp;logo=youtube&amp;labelColor=red" style={{maxWidth:"10ch", verticalAlign:"middle"}}/></a>  
-            </h2>
+          <div id="creds" style={{display:"flex", flexDirection:"row", alignItems:"flex-end", gap:"4px"}}>
+              
+              <a href="https://github.com/Siphon880gh/" rel="nofollow" target="_blank">
+                  <img class="my-badge" src="https://img.shields.io/badge/Github-darkgray?style=flat&amp;logo=github&amp;labelColor=darkgray&amp;logoColor=white" alt="Youtube" data-canonical-src="https://img.shields.io/badge/Github-darkgray?style=flat&amp;logo=youtube&amp;labelColor=lightgray&amp;labelColor=white" style={{display:"inline"}}/>
+              </a>
+
+              <a href="https://www.linkedin.com/in/weng-fung/" rel="nofollow" target="_blank">
+                  <img class="my-badge" src="https://img.shields.io/badge/LinkedIn-blue?style=flat&amp;logo=linkedin&amp;labelColor=blue" alt="Linked-In" data-canonical-src="https://img.shields.io/badge/LinkedIn-blue?style=flat&amp;logo=linkedin&amp;labelColor=blue" style={{display:"inline"}}/>
+              </a>
+
+              <a href="https://www.youtube.com/user/Siphon880yt/" rel="nofollow" target="_blank">
+                  <img class="my-badge" src="https://img.shields.io/badge/Youtube-red?style=flat&amp;logo=youtube&amp;labelColor=red" alt="Youtube" data-canonical-src="https://img.shields.io/badge/Youtube-red?style=flat&amp;logo=youtube&amp;labelColor=red" style={{display:"inline"}}/>
+              </a>
+
+              <a href="https://www.linkedin.com/in/weng-fung/" rel="nofollow" target="_blank">
+                  <img class="my-badge" src="https://img.shields.io/badge/WengIndustry.com-black?style=flat&amp;logo=googlehome&amp;labelColor=black&amp;logoColor=white" alt="Website" data-canonical-src="https://img.shields.io/badge/WengIndustry.com-black?style=flat&amp;logo=googlehome&amp;labelColor=black&amp;logoColor=black" style={{display:"inline"}}/>
+              </a>
+
+              {/* <a href="https://www.paypal.com/donate?business=T42BK25TYPZSA&amp;item_name=Buy+me+coffee+%28I+develop+free+apps%29&amp;currency_code=USD" target="_blank" title="Donate to this project using Buy Me A Coffee" alt="Paypal">
+                  <img class="my-badge" src="https://img.shields.io/badge/buy%20me%20a%20coffee-donate-yellow.svg" alt="Buy Me A Coffee donate button" style={{display:"inline"}}/>
+              </a> */}
+
           </div>
         )
       }
--- src/Welcome.js
+++ src/Welcome.js
@@ -9,6 +9,9 @@ function Welcome() {
         <p>Get to working out with timers and rep counters! Workouts are on the other panel.</p>
         <p>I'm focused mostly on rehab, flexibility, etc, outside of bodybuilding because there are already countless resources on bodybuilding. I find it interesting that people's postures can be corrected with routine exercises. There could also be workouts that burn fat.</p>
         <p>A quick note - A folder shows the number of workouts, eg. (3). Some workouts are premium and only accessed with valid password 🔑, so you may be teased there are more exercises than appears.</p>
+        <p>Check out a workout with full videos, sets/reps, durations:</p>
+        <div style={{height:"20px"}}></div>
+        <a href="view/Flexibility/Posture/Forward%20head.txt"><button style={{cursor:"pointer", background:"lightblue", fontSize:"120%", fontWeight:"bold", border:"0", borderRadius:"10px", padding:"15px"}}>Check it out</button></a>
     </div>
   )
 }
